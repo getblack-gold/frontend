3980c9b536cea03735bcf6decf0e5290b9cc5da5
naomiHauret <hauretnaomi@gmail.com>
2023-07-13 16:33:53 +0200

    chore: install fuel ts sdk

--- apps/shopify-storefront-extension-source/src/App.tsx
+++ apps/shopify-storefront-extension-source/src/App.tsx
@@ -1,6 +1,16 @@
 import type { Component } from 'solid-js'
 import { Routes } from '@solidjs/router'
 import { ScreenAvailableRewards, ScreenDashboard, ScreenEarnRewards, ScreenStatus } from './features'
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverPositioner,
+  PopoverContent,
+  PopoverTitle,
+  PopoverDescription,
+  PopoverCloseTrigger,
+} from '@ark-ui/solid'
+import { Portal } from 'solid-js/web'
 
 const Stack = () => {
   return (
@@ -14,7 +24,23 @@ const Stack = () => {
 }
 
 const App: Component = () => {
-  return <Stack />
+  return (
+    <Popover open={true} portalled>
+      <PopoverTrigger>
+        <button>Open Popover</button>
+      </PopoverTrigger>
+      <Portal>
+        <PopoverPositioner class="max-w-[100dvw] w-full px-3 !min-w-[unset]">
+          <PopoverContent class="border shadow-lg w-96 max-w-full">
+            <Stack />
+            <PopoverCloseTrigger>
+              <button>Close</button>
+            </PopoverCloseTrigger>
+          </PopoverContent>
+        </PopoverPositioner>
+      </Portal>
+    </Popover>
+  )
 }
 
 export default App
